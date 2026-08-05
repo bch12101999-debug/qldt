aa96e4f638f428de168089b0ff4832cd51d0b75d
bch12101999-debug <bch12101999@gmail.com>
2026-08-05 14:49:01 +0700

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,18 @@ app.use(
   }),
 )
 
-app.use(express.json())
+app.use(
+  express.json({
+    limit: '100mb',
+  }),
+)
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '100mb',
+  }),
+)
 
 // Phục vụ file upload
 app.use('/api/uploads', express.static(uploadDir))
